2bcf07cc7ba5b0084b9f73e6e6c2f86f1fc6de4d
Trouni Tiet <trouni@gmail.com>
2022-01-18 11:20:45 +0900

    reboot: add pseudo code for step 1

--- reboot/xmas-list/lib/interface.rb
+++ reboot/xmas-list/lib/interface.rb
@@ -1,1 +1,6 @@
-# TODO: you can build your christmas list program here!
+# 0 - Greet the user
+# 1 - Give the user options (list, add, delete, quit)
+# 2 - Get user input about what they want to do (store it in a variable `action`)
+# 3a - Check what the user wants to do using a case statement
+# 3b - Just add a placeholder inside the action
+# 4 - Loop the required lines of code using a while/until statement
